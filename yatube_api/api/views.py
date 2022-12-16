806ba3a4f58fa5f2dbf0597ddeab7d9451aef09a
Максим Никифоров <max.nnn16@gmail.com>
2022-12-16 19:21:49 +0300

    Создание Api для yatube

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Post, Group, Comment, Follow
-from django.contrib.auth import get_user_model
-
-from rest_framework import permissions, viewsets
+from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from posts.models import Group, Post
 
 from .permissions import IsAuthorOrReadOnly
-from .serializers import (PostSerializer, GroupSerializer,
-                          CommentSerializer, FollowSerializer)
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -24,6 +23,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = (AllowAny,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -41,9 +41,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class FollowViewSet(viewsets.ModelViewSet):
-    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('user__username', 'following__username')
 
     def get_queryset(self):
         user = self.request.user
@@ -52,4 +53,3 @@ class FollowViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
